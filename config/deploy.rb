bebf91d98d2a526189bea3fd6c5f440a1e227b87
dani <dani@mandarine.(none)>
2009-03-18 14:23:04 +0100

    i18n

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,4 @@
-# SOURCES
-# setup users: http://www.viget.com/extend/building-an-environment-from-scratch-with-capistrano-2/
+# SOURCES setup users: http://www.viget.com/extend/building-an-environment-from-scratch-with-capistrano-2/
 # setup deploy: http://www.capify.org/getting-started/from-the-beginning/
 
 default_run_options[:pty] = true  
@@ -12,10 +11,34 @@ set :scm, "git"
 set :repository,  "git@github.com:misspi/calclab.git"
 set :branch, "master"
 set :deploy_via, :remote_cache
-#set :git_shallow_clone, 1
-#set :git_enable_submodules, 1
+set :scm_verbose, true
+# set :git_shallow_clone, 1 #set :git_enable_submodules, 1
 
 role :app, "toami.net"
 role :web, "toami.net"
 role :db,  "toami.net", :primary => true
 
+after "deploy:update_code", "config:copy_shared_configurations"
+
+# Configuration Tasks
+namespace :config do
+  desc "copy shared configurations to current"
+  task :copy_shared_configurations, :roles => [:app] do
+    %w[database.yml].each do |f|
+      run "ln -nsf #{shared_path}/config/#{f} #{release_path}/config/#{f}"
+    end
+  end
+end
+
+
+namespace :deploy do
+  desc "Restarting mod_rails with restart.txt"
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+
+  [:start, :stop].each do |t|
+    desc "#{t} task is a no-op with mod_rails"
+    task t, :roles => :app do ; end
+  end
+end
