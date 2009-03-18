36ed0ec00e1b2ec006b1dd533491e7a48bac08dc
dani <dani@mandarine.(none)>
2009-03-18 11:43:49 +0100

    automatic deployment... part 1 of ?

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,8 +12,8 @@ set :scm, "git"
 set :repository,  "git@github.com:misspi/calclab.git"
 set :branch, "master"
 set :deploy_via, :remote_cache
-set :git_shallow_clone, 1
-set :git_enable_submodules, 1
+#set :git_shallow_clone, 1
+#set :git_enable_submodules, 1
 
 role :app, "toami.net"
 role :web, "toami.net"
